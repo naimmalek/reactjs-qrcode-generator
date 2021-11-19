a851aa32994c07fe84ba5d2fc287c3f1383e6e96
Naim Malek <naimmalek7600@gmail.com>
2021-11-19 21:41:12 +0530

    Updates

---
+++ precache-manifest.717f7390ee611efe65d9f9e5546a8e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b1fb8978009ba26c093443272dfaa9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d9828d0d468200cb5a1",
+    "url": "./static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "d09e6570a97b0b2f4bcf",
+    "url": "./static/css/main.823321c8.chunk.css"
+  },
+  {
+    "revision": "0d9828d0d468200cb5a1",
+    "url": "./static/js/2.24acb9db.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.24acb9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09e6570a97b0b2f4bcf",
+    "url": "./static/js/main.3106a190.chunk.js"
+  },
+  {
+    "revision": "60f1e52b7492a84eccf8",
+    "url": "./static/js/runtime-main.cb47bc7d.js"
+  }
+]);
